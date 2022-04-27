f9682f4251429f02ed9e2fdb4c6293fd4efe1036
Nikhil Bisht <nikhilb@exzeo.net>
2022-04-27 18:47:24 +0530

    added config file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,12 @@ const Papa = require('papaparse')
 
 Cypress.Commands.add('exportCSV', (data) => {
     var csv = Papa.unparse(data);
-    cy.writeFile('D:/automation_practice/automation-test-challenge/cypress/fixtures/test.csv',csv)
+    cy.writeFile('./cypress/fixtures/test.csv',csv)
 
+})
+
+Cypress.Commands.add('login',(email,password)=>{
+    cy.get('#login-form input[name*="login[username]"]').type(email)
+    cy.get('#login-form input#pass').type(password)
+    cy.get("#login-form #send2").click()
 })
